05f9415de9c489f0b8dff54c22c1784b6bbcd4ad
hsps955201 <hsps955201@gmail.com>
2017-11-05 14:16:26 +0800

    Update music.c

--- music.c
+++ music.c
@@ -7,33 +7,33 @@ using UnityEngine;
 public class Audio : NetworkBehaviour
 {
     public AudioSource source;
-        public AudioClip[] clips;
-           
-               // Use this for initialization
-                   void Start () {
-                           source = this.GetComponent<AudioSource>();
-                               }
-                                   public void PlaySound(int id)
-                                       {
-                                               if (id >= 0 && id < clips.Length)
-                                                       {
-                                                                   CmdSendServerSoundID(id);
-                                                                           }
-                                                                               }
+    public AudioClip[] clips;
+   
+    // Use this for initialization
+    void Start () {
+        source = this.GetComponent<AudioSource>();
+    }
+    public void PlaySound(int id)
+    {
+        if (id >= 0 && id < clips.Length)
+        {
+            CmdSendServerSoundID(id);
+        }
+    }
 
-                                                                                  // [Command]
-                                                                                      void CmdSendServerSoundID(int id)
-                                                                                          {
-                                                                                                  RpcSendSoundIDToClients(id);
-                                                                                                      }
-                                                                                                         // [ClientRpc]
-                                                                                                             void RpcSendSoundIDToClients(int id)
-                                                                                                                 {
-                                                                                                                             source.PlayOneShot(clips[id]);
-                                                                                                                                      
-                                                                                                                                          }
-                                                                                                                                              // Update is called once per frame
-                                                                                                                                                  void Update () {
-                                                                                                                                                          //source.PlaySound(0);
-                                                                                                                                                            }
-                                                                                                                                                            }
+   // [Command]
+    void CmdSendServerSoundID(int id)
+    {
+        RpcSendSoundIDToClients(id);
+    }
+   // [ClientRpc]
+    void RpcSendSoundIDToClients(int id)
+    {
+            source.PlayOneShot(clips[id]);
+         
+    }
+    // Update is called once per frame
+    void Update () {
+        //source.PlaySound(0);
+	}
+}
